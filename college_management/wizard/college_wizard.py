1b274f9ed9b33e231d045e7d09a8bb1227a65656
aryan <aryan@icreativetechnolabs.com>
2022-02-15 18:38:24 +0530

    [ADD]college_management

--- college_management/wizard/college_wizard.py
+++ college_management/wizard/college_wizard.py
@@ -1,11 +1,16 @@
+from inspect import signature
+
 from odoo import models, fields, api
 
 class College_Management(models.TransientModel):
     _name = 'college.wizard'
     _description = 'Wizard1'
+
     name = fields.Many2one("res.partner", string="Name")
     mobile = fields.Integer(string="Mobile Number")
     address = fields.Text(string="Address")
+    upload = fields.Binary(string="File Upload")
+    signature = fields.Binary(string="Signature")
 
     def submit_button(self):
         print("Submit Button")
