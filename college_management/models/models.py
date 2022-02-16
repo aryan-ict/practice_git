8330536952932a85ce2f5ffec00ed1b2b1d7f359
aryan <aryan@icreativetechnolabs.com>
2022-02-16 18:56:00 +0530

    [ADD]college_management

--- college_management/models/models.py
+++ college_management/models/models.py
@@ -13,6 +13,7 @@ class college_management(models.Model):
     select_type = fields.Selection([('option1', 'Graduation'), ('option2', 'Post Graduation')],string="Degree Type")
     dob = fields.Date(string="Date Of Birth")
 
+
     def sample_button_d(self):
         print("Sample Button")
 
