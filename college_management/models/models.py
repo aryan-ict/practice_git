3f452e33b2fca7ba4f1265bd49b2132584eb6491
aryan <aryan@icreativetechnolabs.com>
2022-02-17 18:58:50 +0530

    [IMP]college_management

--- college_management/models/models.py
+++ college_management/models/models.py
@@ -8,11 +8,12 @@ class college_management(models.Model):
     _description = 'college_management.college_management'
 
     name = fields.Char()
-    value = fields.Integer()
+    value = fields.Integer(string="Sr Number", default=None)
     description = fields.Text()
     select_type = fields.Selection([('option1', 'Graduation'), ('option2', 'Post Graduation')],string="Degree Type")
     dob = fields.Date(string="Date Of Birth")
-
+    status = fields.Selection([('O1', 'Draft'), ('O2', 'Confirm'), ('O3', 'Done'), ('O4', 'Cancelled')], default='O1',
+                              string="Status")
 
     def sample_button_d(self):
         print("Sample Button")
--- college_management/wizard/college_wizard.py
+++ college_management/wizard/college_wizard.py
@@ -2,6 +2,7 @@ from inspect import signature
 
 from odoo import models, fields, api
 
+
 class College_Management(models.TransientModel):
     _name = 'college.wizard'
     _description = 'Wizard1'
@@ -9,10 +10,16 @@ class College_Management(models.TransientModel):
     name = fields.Many2one("res.partner", string="Name")
     mobile = fields.Integer(string="Mobile Number")
     address = fields.Text(string="Address")
+    country = fields.Many2one('res.country', string='Country')
+    course = fields.Selection([('O1', "Diploma"), ('O2', "Graduation"), ('O3', 'Post Graduation')],
+                              string="Course Type")
     upload = fields.Binary(string="File Upload")
+    interests = fields.Many2many('res.partner.category', string="Your Interests")
     signature = fields.Binary(string="Signature")
+    image = fields.Binary(string="Upload Image")
 
     def submit_button(self):
         print("Submit Button")
+
     def cancel_button(self):
         print("Cancel Button")
