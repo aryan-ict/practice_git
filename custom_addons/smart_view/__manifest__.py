8f22a9203a8b90cb911fbc396e31b29624291cfa
aryan <aryan@icreativetechnolabs.com>
2022-02-28 18:54:28 +0530

    [ADD]Added chatter and made 2 fields trackable and added search view filters and added one magic field in the form view which shows who updated the record

--- custom_addons/smart_view/__manifest__.py
+++ custom_addons/smart_view/__manifest__.py
@@ -17,6 +17,7 @@
     # any module necessary for this one to work correctly
     'depends': [
         'base',
+        'mail',
     ],
     # always loaded
     'data': [
--- custom_addons/smart_view/models/smart.py
+++ custom_addons/smart_view/models/smart.py
@@ -4,14 +4,20 @@ from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 
+
 class smart_view(models.Model):
     _name = "smart.view"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     _description = "Model for Smart View"
 
     name = fields.Char(string="Name")
     phone_no = fields.Char(string="Phone Number")
     dob_id = fields.Date(string="Date Of Birth")
+    gender_id = fields.Selection([('male', 'Male'), ('female', 'Female'), ('transgender', 'Transgender')],
+                                 string="Gender", tracking=True)
+    status_bar = fields.Selection([('apply', 'Applied'), ('wait', 'Waiting'), ('approve', 'Approved')], default="apply",
+                                  tracking=True)
 
     _sql_constraints = [
         ('name_uniq', 'unique (name)', "Name is already in the database."),
@@ -19,6 +25,14 @@ class smart_view(models.Model):
 
     def smart_button(self):
         print("hello")
+
+    '''On the click of log button function will update the phone number and 
+        change the gender and will enter a log note'''
+    def log_button(self):
+        up_vals = {'phone_no': '7575076690',
+                   'gender_id': 'male'}
+        self.message_post(body="Phone number has been updated and Gender has been changed")
+        self.write(up_vals)
 
     # @api.constrains('name')
     # def check_name(self):
