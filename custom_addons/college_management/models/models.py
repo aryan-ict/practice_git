c71a7dfcb09150659574d79d0f4b9364d3cd0480
aryan <aryan@icreativetechnolabs.com>
2022-03-02 19:02:17 +0530

    [ADD][IMP]Added One2many field in smart_view created different model in same module and Improved coding style according to pep8, flake8, pylint

--- custom_addons/college_management/models/models.py
+++ custom_addons/college_management/models/models.py
@@ -1,43 +1,43 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-'''
-Creating a class with name college_management.
-Using models.Model giving the type of model.
-Assigning name for the model which is college_management.college_management.
-Description is about describing the model is about College Management.
-'''
+"""This module is For Practice"""
+from odoo import models, fields
 
 
-class college_management(models.Model):
+class CollegeManagement(models.Model):
+    """This class is for fields and ORM methods"""
     _name = "college.management"
-
     '''Inherited models from base addons'''
     _inherit = ['mail.thread', 'mail.activity.mixin']
-
     _description = "Model for College Management"
-
-    '''Assigning different fields with different fields type.'''
     name = fields.Char(string="Name")
     mobile_no = fields.Char(string="Mobile Number")
     description = fields.Text()
-    select_type = fields.Selection([('option1', 'Graduation'), ('option2', 'Post Graduation')], string="Degree Type",
+    select_type = fields.Selection([('option1', 'Graduation'),
+                                    ('option2', 'Post Graduation')],
+                                   string="Degree Type",
                                    tracking=True)
     dob = fields.Date(string="Date Of Birth")
     status = fields.Selection(
-        [('draft', 'Draft'), ('process', 'In Process'), ('confirm', 'Confirm'), ('cancelled', 'Cancelled')],
+        [('draft', 'Draft'), ('process', 'In Process'),
+         ('confirm', 'Confirm'), ('cancelled', 'Cancelled')],
         default='draft', string="Status", tracking=True)
 
     def sample_button_d(self):
-        pass
+        """Function for sample button
+        unlinks record"""
+        self.unlink()
 
     def create_button(self):
+        """Function for create button
+        creates new values
+        :return: record"""
         vals1 = {'name': 'Kavish',
-                 'value': '03'},
+                 'value': '03'}
         return self.create(vals1)
 
     def write_button(self):
+        """Function for write button
+        writes new values
+        :return: record"""
         vals = {'name': 'Aryan',
                 'value': '02',
                 'description': 'Odoo Trainee',
@@ -46,5 +46,7 @@ class college_management(models.Model):
         return self.write(vals)
 
     def unlink_button(self):
+        """Function for unlink button
+        unlinks the selected record
+        :returns: None"""
         self.unlink()
-'''Simple function for creation of button which prints 'Sample Button' on the click of the button'''
--- custom_addons/smart_view/models/smart.py
+++ custom_addons/smart_view/models/smart.py
@@ -1,39 +1,49 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-from odoo.exceptions import ValidationError
-from datetime import datetime
+"""This module is for Smart View Tasks"""
+from odoo import models, fields
+# from odoo.exceptions import ValidationError
 
 
-
-class smart_view(models.Model):
+class SmartView(models.Model):
+    """This Class is for fields and ORM Functions"""
     _name = "smart.view"
     _inherit = ['mail.thread', 'mail.activity.mixin']
-
     _description = "Model for Smart View"
-
     name = fields.Char(string="Name")
-    phone_no = fields.Char(string="Phone Number", related='names_list.mobile_no')
-    dob_id = fields.Datetime(string="Date Of Birth", default=fields.Datetime.now)
-    gender_id = fields.Selection([('male', 'Male'), ('female', 'Female'), ('transgender', 'Transgender')],
+    phone_no = fields.Char(string="Phone Number",
+                           related='names_list.mobile_no')
+    doc = fields.Datetime(string="Date Of Creation",
+                          default=fields.Datetime.now)
+    gender_id = fields.Selection([('male', 'Male'),
+                                  ('female', 'Female'),
+                                  ('transgender', 'Transgender')],
                                  string="Gender", tracking=True)
-    status_bar = fields.Selection([('apply', 'Applied'), ('wait', 'Waiting'), ('approve', 'Approved')], default="apply",
+    status_bar = fields.Selection([('apply', 'Applied'),
+                                   ('wait', 'Waiting'),
+                                   ('approve', 'Approved')],
+                                  default="apply",
                                   tracking=True)
-    names_list = fields.Many2one('college.management', string="Name List")
+    names_list = fields.Many2one('college.management',
+                                 string="Name List")
+    first_page = fields.One2many('smart.view.otm', 'appointment_id',
+                                        string='Appointment Lines')
 
     _sql_constraints = [
-        ('name_uniq', 'unique (name)', "Name is already in the database."),
+        ('name_uniq', 'unique (name)', "Name is already in the database.")
     ]
 
     def smart_button(self):
-        print("hello")
+        """This function is for smart button"""
+        return self
 
-    '''On the click of log button function will update the phone number and 
-        change the gender and will enter a log note'''
     def log_button(self):
+        """This function is for log button
+        and posts a message in chatter on the click
+        of the button and updates values.
+        """
         up_vals = {'phone_no': '7575076690',
                    'gender_id': 'male'}
-        self.message_post(body="Phone number has been updated and Gender has been changed")
+        self.message_post(body="Phone number has been updated "
+                               "and Gender has been changed")
         self.write(up_vals)
 
     # @api.constrains('name')
@@ -47,3 +57,14 @@ class smart_view(models.Model):
     #     for record in self:
     #         if len(record.phone_no) > 10 or len(record.phone_no) < 10:
     #             raise ValidationError("The number must be 10 digits long")
+
+
+class SmartViewOtm(models.Model):
+    """This class is for One2many field"""
+    _name = "smart.view.otm"
+    _description = "Smart View One2many"
+    _rec_name = 'test_list'
+    product_id = fields.Many2one('product.product', string='Product')
+    product_qty = fields.Integer(string='Product Quantity')
+    appointment_id = fields.Many2one('smart.view', string='Appointment ID')
+    test_list = fields.Many2one('smart.view.otm', string='Test')
